a0767e9f7e5dd8de3b968f07cbc1a60c5b0713b8
Tosikmosik <card_gamit@mil.ru>
2022-09-16 16:37:27 +0300

    домашняя работа 25

---
+++ задача 29/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив
+// из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+Console.Clear();
+int[] arr = new int[8];
+
+void ArrEight(int[] arr1)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr1[i] = new Random().Next(0, 29);
+    }
+}
+void PrintArray(int[] arr1)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1)
+            Console.Write($"{arr1[i]},");
+        else
+            Console.Write($"{arr1[i]}");
+    }
+
+    Console.Write("]");
+}
+ArrEight(arr);
+PrintArray(arr);
--- семинар/Program.cs
+++ семинар/Program.cs
@@ -1,16 +1,26 @@
 ﻿Console.Clear();
-Console.WriteLine("Введите число: ");
-int number = Convert.ToInt32(Console.Read());
+string[,] table = new string[2, 5];
+// table[1, 2] = "слово";
+// for (int rows = 0; rows < 2; rows++)
+// {
+//     for (int columns = 0; columns < 5; columns++)
+//     {
+//         Console.WriteLine($"-{table[rows, columns]}-");
+//     }
+// }
 
-int SumNumbers(int num)
+int[,] matrix = new int[3, 4];
+
+void PrintArray(int[,] matr)
 {
-    int sum = default;
-    for (int i = 1; i <= num; i++)
-    {
-        sum += i;
-    }
-    return sum;
-}
-int sumNumbers = SumNumbers(number);
-Console.WriteLine($"Сумма чисел от 1 до {number} = {sumNumbers}");
 
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+}
